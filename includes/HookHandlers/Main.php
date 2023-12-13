3bc3e66e766061aa75eed2d208bf34a86a632e49
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-13 12:38:36 -0700

    Add Main handler

---
+++ includes/HookHandlers/Main.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace WikiForge\RottenLinks\HookHandlers;
+
+use JobQueueGroup;
+use MediaWiki\Deferred\LinksUpdate\LinksUpdate;
+use MediaWiki\Hook\LinksUpdateCompleteHook;
+use WikiForge\RottenLinks\RottenLinksJob;
+
+class Main implements LinksUpdateCompleteHook {
+
+
+	/** @var JobQueueGroup */
+	private $jobQueueGroup;
+
+	/**
+	 * @param JobQueueGroup $jobQueueGroup
+	 */
+	public function __construct( JobQueueGroup $jobQueueGroup ) {
+		$this->jobQueueGroup = $jobQueueGroup;
+	}
+
+	/**
+	 * Handler for LinksUpdateComplete hook.
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/LinksUpdateComplete
+	 * @param LinksUpdate $linksUpdate
+	 * @param mixed $ticket
+	 */
+	public function onLinksUpdateComplete( $linksUpdate, $ticket ) {
+		$addedExternalLinks = $linksUpdate->getAddedExternalLinks();
+		$removedExternalLinks = $linksUpdate->getRemovedExternalLinks();
+
+		if ( $addedExternalLinks || $removedExternalLinks ) {
+			$params = [
+				'addedExternalLinks' => $addedExternalLinks,
+				'removedExternalLinks' => $removedExternalLinks
+			];
+
+			$this->jobQueueGroup->push( new RottenLinksJob( $params ) );
+		}
+	}
+}
